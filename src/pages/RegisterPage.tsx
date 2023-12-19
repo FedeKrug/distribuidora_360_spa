ad2847f95e1ed5fc20788ab605692b05db022058
FedeKrug <86620474+FedeKrug@users.noreply.github.com>
2023-12-19 16:14:02 -0300

    Update RegisterPage.tsx

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -109,7 +109,7 @@ export const RegisterPage = () => {
 
                             <button
                                 data-mdb-ripple-init
-                                type='button'
+                                type="button"
                                 className="btn btn-primary btn-block mb-4 "
                                 onClick={signUp}
                                 disabled={isAuthenticating}
@@ -125,36 +125,8 @@ export const RegisterPage = () => {
         </div>
 
 
-        //   <div className={`nc-PageSignUp  ${className}`} data-nc-id="PageSignUp">
-        //     <div className="container mb-24 lg:mb-32">
-        //       <h2 className="my-20 flex items-center text-3xl leading-[115%] md:text-5xl md:leading-[115%] font-semibold text-neutral-900 dark:text-neutral-100 justify-center">
-        //         {translate(tr.Signup)}
-        //       </h2>
-        //       <div className="max-w-md mx-auto space-y-6 ">
-        //         <div className="grid gap-3">
-        //           <ButtonPrimary
-        //             onClick={googleSignIn}
-        //             className="nc-will-change-transform flex w-full rounded-lg bg-primary-50 dark:bg-neutral-800 px-4 py-3 transform transition-transform sm:px-6 hover:translate-y-[-2px]"
-        //             disabled={isAuthenticating}
-        //           >
-        //             <img
-        //               className="flex-shrink-0"
-        //               src={googleSvg}
-        //               alt={'Google Sign in'}
-        //             />
-        //             <h3 className="flex-grow text-center text-sm font-medium text-neutral-700 dark:text-neutral-300 sm:text-sm">
-        //               {translate(tr.ContinueWithGoogle)}
-        //             </h3>
-        //           </ButtonPrimary>
-        //         </div>
-        //         {/* OR */}
-        //         <div className="relative text-center">
-        //           <span className="relative z-10 inline-block px-4 font-medium text-sm bg-white dark:text-neutral-400 dark:bg-neutral-900">
-        //             {translate(tr.Or)}
-        //           </span>
-        //           <div className="absolute left-0 w-full top-1/2 transform -translate-y-1/2 border border-neutral-100 dark:border-neutral-800"></div>
-        //         </div>
-        //         {/* FORM */}
+
+
         //         <form className="grid grid-cols-1 gap-6">
         //           <label className="block">
         //             <span className="text-neutral-800 dark:text-neutral-200">
